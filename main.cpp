c46e1019f350b4c7118373714d86b3d37e844738
TashinAhamed <83556321+TashinAhamed@users.noreply.github.com>
2021-05-03 02:53:40 +0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+int fib(int n){
+    if (n<=2) return 1;
+    return fib(n-1)+fib(n-2);
+}
+int main() {
+    printf("%d",fib(8));
+}
